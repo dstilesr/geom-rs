e5e65bfd31a8020f466f9059ead776b9b967df69
dstilesr <d.stiles.r@hotmail.com>
2025-11-10 19:23:49 -0500

    Additional test for cvx hull

--- src/geom/cvx.rs
+++ src/geom/cvx.rs
@@ -50,6 +50,7 @@ fn half_hull(points: Iter<Point>) -> Vec<Point> {
 #[cfg(test)]
 mod tests {
     use super::*;
+    use rand::{Rng, rng};
 
     #[test]
     fn test_cvx_hull_simple() {
@@ -73,6 +74,24 @@ mod tests {
             assert_eq!(poly.points[3].coords(), (1.0, 0.0));
         } else {
             panic!("Failed to instantiate convex hull!");
+        }
+    }
+
+    #[test]
+    fn test_convex_hull_random() {
+        let mut random = rng();
+        let total_points = 350;
+        let mut raw_pts = Vec::new();
+        for _ in 0..total_points {
+            // Create a bunch of random points
+            raw_pts.push(Point::new(random.random(), random.random()));
+        }
+        let hull = convex_hull(&raw_pts);
+        match hull {
+            Some(poly) => {
+                assert!(poly.points.len() <= (total_points + 1));
+            }
+            None => panic!("Could not instantiate convex hull of random points"),
         }
     }
 }
